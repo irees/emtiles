b074da64861b82fc46668bde89e62918ffbe715d
Ian <ian@ianrees.net>
2014-04-06 13:16:37 -0700

    Start working on EMTiles server

---
+++ server.py
@@ -0,0 +1,12 @@
+"""EMTiles-aware MBTiles Server."""
+
+import twisted
+import twisted.web
+import json
+import sqlite3
+
+class EMTileServer(object):
+    pass
+    
+if __name__ == "__main__":
+    pass
